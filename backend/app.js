4ce9d634a8f7cf3c32275798afab4cf2a9ba8793
JORNAT <jornat.jerome@gmail.com>
2022-09-24 11:06:41 -0400

    Connexion BD

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+mongoose.connect('mongodb+srv://TCox972:Mafiouz97233@cluster0.ygrzs.mongodb.net/cheDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express()
 
+app.use(express.json())
+app.use(cors())
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
 
 module.exports = app
